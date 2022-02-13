31680c24b929f9709696801a27b14bcd3af1c42b
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2022-02-13 17:02:41 +0000

    Second push

---
+++ s3_bucket_file_upload/automatic_s3_uploader.py
@@ -0,0 +1,8 @@
+from secrets import access_key,secret_access_key
+
+import boto3
+import os
+
+client = boto3.client('s3',
+                        aws_access_key_id = access_key,
+                        aws_secret_access_key = secret_access_key)
---
+++ s3_bucket_file_upload/secrets.py
@@ -0,0 +1,2 @@
+access_key = 'AKIASO5RBB6LBBSPX2KI'
+secret_access_key = '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'
