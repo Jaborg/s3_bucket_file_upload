c165230536e804054a8659d332cbcce621a5dd44
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2022-02-13 21:34:45 +0000

    Completed S3 upload

--- s3_bucket_file_upload/automatic_s3_uploader.py
+++ s3_bucket_file_upload/automatic_s3_uploader.py
@@ -1,8 +1,16 @@
-from secrets import access_key,secret_access_key
-
 import boto3
 import os
 
+
 client = boto3.client('s3',
-                        aws_access_key_id = access_key,
-                        aws_secret_access_key = secret_access_key)
+                        aws_access_key_id = os.getenv('AWS_ACCESS_KEY'),
+                        aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY'))
+
+
+
+
+for file in os.listdir():
+    if '.csv' in file:
+        upload_file_bucket = 'jacobbucketfirst'
+        upload_file_key = 'data/' + str(file)
+        client.upload_file(file,upload_file_bucket,upload_file_key)
--- s3_bucket_file_upload/get_data_api.py
+++ s3_bucket_file_upload/get_data_api.py
@@ -1,11 +1,23 @@
 import requests
+import csv
+
 
 response = requests.get("https://openlibrary.org/search/authors.json?q=t%20hobbes")
 
 
-ids = ['OL50870A','OL4586796A','OL22242A']
+ids = {'OL50870A':'Hobbes','OL4586796A':'Marquez','OL22242A':'Dostoyevsky'}
 
 
-for id in ids[:1]:
+for id,author in ids.items():
     response = requests.get("https://openlibrary.org/authors/{id}/works.json".format(id=id))
-    print(response.json()['entries'][0])
+    data = response.json()['entries']
+
+
+    with open(f'{author}.csv', 'w', encoding='UTF-8') as f:
+
+        writer = csv.DictWriter(f, fieldnames=['Works','Created'])
+        # write the data
+        writer.writeheader()
+
+        for work in data:
+            writer.writerow({'Works': work['title'],'Created': work['created']['value']})
